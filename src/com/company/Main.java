c91c19387a38e42c6968189d9dee859d643f85f0
devashree mahangade <devashreemahangade@gmail.com>
2022-11-17 22:03:34 +0530

    ok

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hellooooooo");
-        System.out.println("hellooooooo");
-        System.out.println("hellooooooo");
-
-        System.out.println("hellooooooo");
-        System.out.println("hellooooooo");
-
-        System.out.println("hellooooooo");
-        System.out.println("hellooooooo");
-        System.out.println("hellooooooo");
-
-        System.out.println("hellooooooo");
-        System.out.println("hellooooooo");
+        
     }
 }
