3acf6c8226fcedc7a9902bced213d28e2fae81d7
devashree mahangade <devashreemahangade@gmail.com>
2022-11-17 22:15:25 +0530

    okk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hellooooooo");
-        
+
+        System.out.println("hellooooo");
+        System.out.println("hi");
+
     }
 }
