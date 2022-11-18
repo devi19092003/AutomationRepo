77cf1750981c3ecd7e4b45b72523089a2dfcd42f
devashree mahangade <devashreemahangade@gmail.com>
2022-11-18 20:48:21 +0530

    new changes are made today

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-      
+        System.out.println("hi");
         System.out.println("new package");
+        System.out.println("making changes");
 
     }
 }
